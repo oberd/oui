05ba70638050fe667d2a44c71bb30441a4cdaec3
Konou <konou@Konous-MacBook-Pro.local>
2019-12-09 14:24:21 -0600

    resetted index html

--- src/components.d.ts
+++ src/components.d.ts
@@ -15,18 +15,6 @@ import {
 } from './components/svg/svgs';
 
 export namespace Components {
-  interface NavButton {}
-  interface NavButtonName {}
-  interface NavDrawer {
-    'open': () => Promise<void>;
-    'opened': boolean;
-    'position': string;
-    'size': string;
-    'title': string;
-  }
-  interface NavLinks {
-    'links': string[];
-  }
   interface OuiCard {}
   interface OuiCardHeading {}
   interface OuiDocumentsIcon {
@@ -40,7 +28,6 @@ export namespace Components {
     */
     'accept': string;
   }
-  interface OuiFileUploadModal {}
   interface OuiSvg {
     'name': SvgPack;
     'scale': number;
@@ -49,30 +36,6 @@ export namespace Components {
 
 declare global {
 
-
-  interface HTMLNavButtonElement extends Components.NavButton, HTMLStencilElement {}
-  var HTMLNavButtonElement: {
-    prototype: HTMLNavButtonElement;
-    new (): HTMLNavButtonElement;
-  };
-
-  interface HTMLNavButtonNameElement extends Components.NavButtonName, HTMLStencilElement {}
-  var HTMLNavButtonNameElement: {
-    prototype: HTMLNavButtonNameElement;
-    new (): HTMLNavButtonNameElement;
-  };
-
-  interface HTMLNavDrawerElement extends Components.NavDrawer, HTMLStencilElement {}
-  var HTMLNavDrawerElement: {
-    prototype: HTMLNavDrawerElement;
-    new (): HTMLNavDrawerElement;
-  };
-
-  interface HTMLNavLinksElement extends Components.NavLinks, HTMLStencilElement {}
-  var HTMLNavLinksElement: {
-    prototype: HTMLNavLinksElement;
-    new (): HTMLNavLinksElement;
-  };
 
   interface HTMLOuiCardElement extends Components.OuiCard, HTMLStencilElement {}
   var HTMLOuiCardElement: {
@@ -98,43 +61,21 @@ declare global {
     new (): HTMLOuiFileUploadElement;
   };
 
-  interface HTMLOuiFileUploadModalElement extends Components.OuiFileUploadModal, HTMLStencilElement {}
-  var HTMLOuiFileUploadModalElement: {
-    prototype: HTMLOuiFileUploadModalElement;
-    new (): HTMLOuiFileUploadModalElement;
-  };
-
   interface HTMLOuiSvgElement extends Components.OuiSvg, HTMLStencilElement {}
   var HTMLOuiSvgElement: {
     prototype: HTMLOuiSvgElement;
     new (): HTMLOuiSvgElement;
   };
   interface HTMLElementTagNameMap {
-    'nav-button': HTMLNavButtonElement;
-    'nav-button-name': HTMLNavButtonNameElement;
-    'nav-drawer': HTMLNavDrawerElement;
-    'nav-links': HTMLNavLinksElement;
     'oui-card': HTMLOuiCardElement;
     'oui-card-heading': HTMLOuiCardHeadingElement;
     'oui-documents-icon': HTMLOuiDocumentsIconElement;
     'oui-file-upload': HTMLOuiFileUploadElement;
-    'oui-file-upload-modal': HTMLOuiFileUploadModalElement;
     'oui-svg': HTMLOuiSvgElement;
   }
 }
 
 declare namespace LocalJSX {
-  interface NavButton {}
-  interface NavButtonName {}
-  interface NavDrawer {
-    'opened'?: boolean;
-    'position'?: string;
-    'size'?: string;
-    'title'?: string;
-  }
-  interface NavLinks {
-    'links'?: string[];
-  }
   interface OuiCard {}
   interface OuiCardHeading {}
   interface OuiDocumentsIcon {
@@ -152,27 +93,16 @@ declare namespace LocalJSX {
     */
     'onDropped'?: (event: CustomEvent<FileDropEvent>) => void;
   }
-  interface OuiFileUploadModal {
-    /**
-    * Emitted when modal is closed via button or esc
-    */
-    'onClose'?: (event: CustomEvent<any>) => void;
-  }
   interface OuiSvg {
     'name'?: SvgPack;
     'scale'?: number;
   }
 
   interface IntrinsicElements {
-    'nav-button': NavButton;
-    'nav-button-name': NavButtonName;
-    'nav-drawer': NavDrawer;
-    'nav-links': NavLinks;
     'oui-card': OuiCard;
     'oui-card-heading': OuiCardHeading;
     'oui-documents-icon': OuiDocumentsIcon;
     'oui-file-upload': OuiFileUpload;
-    'oui-file-upload-modal': OuiFileUploadModal;
     'oui-svg': OuiSvg;
   }
 }
@@ -183,15 +113,10 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
   export namespace JSX {
     interface IntrinsicElements {
-      'nav-button': LocalJSX.NavButton & JSXBase.HTMLAttributes<HTMLNavButtonElement>;
-      'nav-button-name': LocalJSX.NavButtonName & JSXBase.HTMLAttributes<HTMLNavButtonNameElement>;
-      'nav-drawer': LocalJSX.NavDrawer & JSXBase.HTMLAttributes<HTMLNavDrawerElement>;
-      'nav-links': LocalJSX.NavLinks & JSXBase.HTMLAttributes<HTMLNavLinksElement>;
       'oui-card': LocalJSX.OuiCard & JSXBase.HTMLAttributes<HTMLOuiCardElement>;
       'oui-card-heading': LocalJSX.OuiCardHeading & JSXBase.HTMLAttributes<HTMLOuiCardHeadingElement>;
       'oui-documents-icon': LocalJSX.OuiDocumentsIcon & JSXBase.HTMLAttributes<HTMLOuiDocumentsIconElement>;
       'oui-file-upload': LocalJSX.OuiFileUpload & JSXBase.HTMLAttributes<HTMLOuiFileUploadElement>;
-      'oui-file-upload-modal': LocalJSX.OuiFileUploadModal & JSXBase.HTMLAttributes<HTMLOuiFileUploadModalElement>;
       'oui-svg': LocalJSX.OuiSvg & JSXBase.HTMLAttributes<HTMLOuiSvgElement>;
     }
   }
