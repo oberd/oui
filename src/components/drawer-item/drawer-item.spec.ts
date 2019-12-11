ee8f34ba6f819b20444f5bf185b5b1f65a1d7feb
Eli Vargas <eevargas@gmail.com>
2019-12-11 16:28:48 -0600

    Add drawer-item test

--- src/components/drawer-item/drawer-item.spec.ts
+++ src/components/drawer-item/drawer-item.spec.ts
@@ -14,4 +14,44 @@ describe("Test oui-modal component", () => {
 
     expect(content[0].textContent).toBe("Hello Test")
   })
+
+  it("should render oui-draw-item as a link (a tag)", async () => {
+    const page = await newSpecPage({
+      components: [DrawerItem],
+      html: `<oui-drawer-item label="Hello Test" link="https://ubuntu.com" />`,
+    })
+
+    const content = page.doc
+      .querySelector("oui-drawer-item")
+      .querySelector("a")
+
+    expect(content).not.toBeNull()
+  })
+
+  it("should render oui-draw-item with the a tag href to match link prop", async () => {
+    const page = await newSpecPage({
+      components: [DrawerItem],
+      html: `<oui-drawer-item label="Hello Test" link="https://ubuntu.com" />`,
+    })
+
+    const content = page.doc
+      .querySelector("oui-drawer-item")
+      .querySelector("a")
+
+    expect(content.href).toBe("https://ubuntu.com/")
+  })
+
+  it("should render oui-draw-item with with target and rel attributes", async () => {
+    const page = await newSpecPage({
+      components: [DrawerItem],
+      html: `<oui-drawer-item label="Hello Test" link="https://ubuntu.com" external />`,
+    })
+
+    const content = page.doc
+      .querySelector("oui-drawer-item")
+      .querySelector("a")
+
+    const attrVals = `${content.getAttribute("target")} ${content.getAttribute("rel")}`
+    expect(attrVals).toBe("_blank noopener noreferrer")
+  })
 })
