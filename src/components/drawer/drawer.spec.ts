684c263b22f0cefac7399ce2ef7298a80c99d4f2
Konou <konou@Konous-MacBook-Pro.local>
2019-12-12 16:15:20 -0600

    Test for drawer title functionality pass

---
+++ src/components/drawer/drawer.spec.ts
@@ -0,0 +1,16 @@
+import { newSpecPage } from "@stencil/core/testing"
+import { Drawer } from "./drawer"
+
+describe("Test oui-drawer component", () => {
+    it("should render oui-drawer with drawer-title", async () => {
+      const page = await newSpecPage({
+        components: [Drawer],
+        html: `<oui-drawer drawer-title="Hello Test" />`,
+      })
+
+      const cllp = page.doc.querySelector("oui-drawer") as HTMLOuiDrawer
+      const title = cllp.querySelector(".oui-drawer__title") as HTMLElement
+
+      expect(title.textContent).toBe("Hello Test")
+    })
+  })
