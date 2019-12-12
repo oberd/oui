1342d321e87620081fb8990f306e7af99860cf4a
Eli Vargas <eevargas@gmail.com>
2019-12-12 15:03:29 -0600

    test accordion

--- src/components/accordion/accordion.spec.ts
+++ src/components/accordion/accordion.spec.ts
@@ -1,4 +1,5 @@
 import { newSpecPage } from "@stencil/core/testing"
+import { Collapsable } from "../collapsable/collapsable"
 import { Accordion } from "./accordion"
 
 describe("Test oui-modal component", () => {
@@ -14,15 +15,47 @@ describe("Test oui-modal component", () => {
   })
 
   it("should esc key fire close event", async () => {
-    const eventSpy = jest.fn()
-    const escEvent = new CustomEvent("expand")
     const page = await newSpecPage({
-      components: [Accordion],
-      html: `<oui-accordion />`,
+      components: [Accordion, Collapsable],
+      html: `
+      <oui-accordion>
+        <oui-collapsable class="one" label="Collapsable One">
+          <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud e
+          </p>
+        </oui-collapsable>
+        <oui-collapsable class="two" label="Collapsable Two">
+          <p>
+              entore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
+          </p>
+        </oui-collapsable>
+      </oui-accordion>
+      `,
     })
 
-    // const modal = page.doc.querySelector("oui-modal")
-    // page.doc.dispatchEvent(escEvent)
-    // expect(eventSpy).toHaveBeenCalled()
+    const cllp1: HTMLOuiCollapsableElement = page.doc
+      .querySelector("oui-accordion")
+      .querySelector("oui-collapsable.one") as HTMLOuiCollapsableElement
+
+    const cllp2: HTMLOuiCollapsableElement = page.doc
+      .querySelector("oui-accordion")
+      .querySelector("oui-collapsable.two") as HTMLOuiCollapsableElement
+
+    const title1 = cllp1.querySelector(".oui-collapsable__title") as HTMLElement
+    const title2 = cllp2.querySelector(".oui-collapsable__title") as HTMLElement
+
+    // click on the first collapsable to expand it
+    title1.click()
+
+    // check that the first collapsable is expanded
+    page.waitForChanges()
+    expect(cllp1.expanded).toBe(true)
+
+    // click on the second collapsable to expand it
+    title2.click()
+
+    // check if the first collapsable has collapsed
+    page.waitForChanges()
+    expect(cllp1.expanded).toBe(false)
   })
 })
