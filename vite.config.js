ff61c6b853718ae41d2ed0373fbdf6fa89e0c55e
Nathan Bleigh <nathan.bleigh@gmail.com>
2025-02-20 18:52:55 -0600

    update build for multi-package

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    emptyOutDir: false,
     lib: {
       name: "oui",
       entry: resolve(__dirname, "src/oui.js"),
+      fileName: "oui",
     },
   },
 });
--- vite.config.video.js
+++ vite.config.video.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    emptyOutDir: false,
     lib: {
       name: "oui-video-slice",
       entry: resolve(__dirname, "src/video-slice.js"),
